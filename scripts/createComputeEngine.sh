56244a265db0480a24faf8ce0acd2eafa5acef1e
Chandra Nakkalakunta <chandra.n.gcp@gmail.com>
2023-08-26 23:01:24 +0530

    commit of updated files

--- scripts/createComputeEngine.sh
+++ scripts/createComputeEngine.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+source ../config/cloudvariables.txt
+
+counter=1
+
+while [ $counter -le $1 ]
+do
+    gcloud compute instances create $INSTANCE_NAME-$counter \
+        --project=$PROJECT_ID \
+        --image-family=$IMAGE_FAMILY \
+        --image-project=$IMAGE_PROJECT \
+        --zone=$R3_Z1 \
+        --machine-type=$MACHINE_TYPE \
+        --boot-disk-size=$BOOT_DISK_SIZE \
+        --network=$CUSTOM_VPC_NAME \
+        --subnet=$SN_3_NAME \
+        --stack-type=$STACK_TYPE 
+
+    ((counter++))
+
+done
+
