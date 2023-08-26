3a39382bc5504a626c90d574e9aae30bb4f2ff97
Chandra Nakkalakunta <chandra.n.gcp@gmail.com>
2023-08-26 17:52:02 +0530

    updates to new files

--- scripts/createGCloudProject.sh
+++ scripts/createGCloudProject.sh
@@ -4,4 +4,6 @@ source cloudvariables.txt
 
 gcloud projects create $PROJECT_ID
 
+gcloud billing projects link $PROJECT_ID --billing-account=$BILLING_ACCOUNT
+
 
---
+++ scripts/deleteGCloudProject.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+source cloudvariables.txt
+
+gcloud projects delete $PROJECT_ID
